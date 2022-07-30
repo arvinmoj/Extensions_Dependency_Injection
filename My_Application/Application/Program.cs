c1f9935a184e36658a87d5e8cc010ce8f3f14f8d
Arvinmoj <arvinmojaverian@hotmail.com>
2022-07-30 13:40:10 +0430

    program dependency injection extention

---
+++ My_Application/Application/Program.cs
@@ -0,0 +1,79 @@
+﻿# region Using
+using Microsoft.Extensions.DependencyInjection;
+# endregion \ Using
+
+namespace Application
+{
+    public static class Program : Object
+    {
+        static Program()
+        {
+        }
+
+        public static void Main()
+        {
+
+            #region Pure DependencyInjection
+            // Logger.Interfaces.ILogger
+            //     logger = new Logger.Logger();
+
+            // Logger.SomeClass someClass =
+            //     new Logger.SomeClass(logger: logger);
+
+            // string fullName = "Senior Martian";
+            // someClass.DoSomething(fullName: fullName);
+            # endregion \ Pure DependencyInjection
+
+            RegisterServices();
+
+            IServiceScope scope = MyServiceProvider.CreateScope();
+            string fullName = "Senior Martian";
+            scope.ServiceProvider.GetRequiredService<Logger.SomeClass>()
+                .DoSomething(fullName: fullName);
+
+            DisposeServiceProvider();
+
+            #region Console Read Line 
+            Console.WriteLine("Press [Enter] To Exit ...");
+            Console.ReadLine();
+            # endregion \ Console Read Line 
+
+        }
+
+        // Service Provider
+        private static ServiceProvider MyServiceProvider { get; set; }
+
+        // Register Services
+        # region RegisterServicesFunction
+        private static void RegisterServices()
+        {
+
+            var services = new ServiceCollection();
+
+            // services.AddScoped<Logger.SomeClass>();
+            // services.AddScoped<Logger.Interfaces.ILogger, Logger.Logger>();
+
+            // services.AddSingleton<Logger.SomeClass>();
+            // services.AddSingleton<Logger.Interfaces.ILogger , Logger.Logger>();
+
+            services.AddTransient<Logger.SomeClass>();
+            services.AddTransient<Logger.Interfaces.ILogger, Logger.Logger>();
+
+            MyServiceProvider =
+                services.BuildServiceProvider(validateScopes: true);
+
+        }
+        # endregion \ RegisterServicesFunction
+
+        // Dispose Service Provider
+        #region  DisposeServiceProvider
+        private static void DisposeServiceProvider()
+        {
+            if (MyServiceProvider != null)
+            {
+                MyServiceProvider.Dispose();
+            }
+        }
+        #endregion \ DisposeServiceProvider
+    }
+}
--- My_Application/Logger/SomeClass.cs
+++ My_Application/Logger/SomeClass.cs
@@ -2,6 +2,14 @@
 {
     public class SomeClass : Object
     {
+
+        //public SomeClass(Interfaces.ILogger Logger) : base()
+        //{
+        //    _logger = Logger;
+        //}
+        //private readonly Interfaces.ILogger _logger;
+
+        // Best Practice
         public SomeClass(Interfaces.ILogger logger) : base()
         {
             Logger = logger;
